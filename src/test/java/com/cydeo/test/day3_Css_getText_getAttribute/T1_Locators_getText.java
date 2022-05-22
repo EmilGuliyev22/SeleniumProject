6323a5534bb8802410f2c6398e7f3d339c0ec7a6
Aysun120 <aysundamar114@gmail.com>
2022-05-22 10:33:52 -0500

    T1_Locators_getText

--- src/test/java/com/cydeo/test/day3_Css_getText_getAttribute/T1_Locators_getText.java
+++ src/test/java/com/cydeo/test/day3_Css_getText_getAttribute/T1_Locators_getText.java
@@ -26,9 +26,25 @@ public class T1_Locators_getText {
         WebElement password = driver.findElement(By.name("USER_PASSWORD"));
         password.sendKeys("incorrect");
 
-
 //        5- Click to login button.
+        WebElement loginBtn = driver.findElement(By.className("login-btn"));
+        loginBtn.click();
+
 //        6- Verify error message text is as expected:
 //        Expected: Incorrect login or password
+        WebElement errorMsg = driver.findElement(By.className("errortext"));
+
+        String expectedText = "Incorrect login or password";
+
+        // getText() method; it will get the content(text) form in between the opening and closing tag
+        String actualText =  errorMsg.getText();
+
+        if(actualText.equals(expectedText)){
+            System.out.println("Error message verification passed!");
+        }else{
+            System.out.println("Error message verification failed!");
+        }
+
+     driver.quit();
     }
 }
