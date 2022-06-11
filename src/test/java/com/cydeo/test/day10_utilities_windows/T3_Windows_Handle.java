e2cc4fad96f19471a0b2091e4383634fe48f4f41
Aysun120 <aysundamar114@gmail.com>
2022-06-11 14:21:17 -0500

    T3_Windows_Handle class added

--- src/test/java/com/cydeo/test/day10_utilities_windows/T3_Windows_Handle.java
+++ src/test/java/com/cydeo/test/day10_utilities_windows/T3_Windows_Handle.java
@@ -1,6 +1,9 @@
 package com.cydeo.test.day10_utilities_windows;
 
 import com.cydeo.test.base.TestBase;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class T3_Windows_Handle extends TestBase {
@@ -9,12 +12,19 @@ public class T3_Windows_Handle extends TestBase {
     public void windowHandleTest(){
 
 //        2. Go to : https://practice.cydeo.com/windows
+        driver.get("https://practice.cydeo.com/windows");
 
 //        3. Assert: Title is “Windows”
+        String actualTitle = driver.getTitle();
+        String expectedTitle = "Windows";
+        Assert.assertEquals(actualTitle,expectedTitle,"Main window title verification failed!");
 
 //        4. Click to: “Click Here” link
+        WebElement clickHereLink = driver.findElement(By.linkText("Click Here"));
+        clickHereLink.click();
 
 //        5. Switch to new Window.
+
 
 //        6. Assert: Title is “New Window”
 
