f4c8082242143fcf742cf394982d28c723a7e063
Aysun120 <aysundamar114@gmail.com>
2022-06-11 15:00:39 -0500

    T3_Windows_Handle class added

--- src/test/java/com/cydeo/test/day10_utilities_windows/T3_Windows_Handle.java
+++ src/test/java/com/cydeo/test/day10_utilities_windows/T3_Windows_Handle.java
@@ -6,6 +6,8 @@ import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import java.util.Set;
+
 public class T3_Windows_Handle extends TestBase {
 
     @Test
@@ -13,6 +15,13 @@ public class T3_Windows_Handle extends TestBase {
 
 //        2. Go to : https://practice.cydeo.com/windows
         driver.get("https://practice.cydeo.com/windows");
+
+        String mainWindow = driver.getWindowHandle();
+        System.out.println("mainWindow = " + mainWindow);
+
+//   mainWindow = CDwindow-C536E9E39AF57A504F13D7D931E2CC62
+//   mainWindow = CDwindow-7F7475D366ACC60228B028F59F7793D6
+
 
 //        3. Assert: Title is “Windows”
         String actualTitle = driver.getTitle();
@@ -24,9 +33,19 @@ public class T3_Windows_Handle extends TestBase {
         clickHereLink.click();
 
 //        5. Switch to new Window.
-
+        Set<String> allWindowHandles = driver.getWindowHandles();
+//
+        for (String each : allWindowHandles) {
+            driver.switchTo().window(each);
+            // window handle 1 - main window
+            // window handle 2 - 2nd window
+            System.out.println("driver.getTitle() = " + driver.getTitle());
+        }
 
 //        6. Assert: Title is “New Window”
+        String expectTitleAfterClicking = "New Window";
+        actualTitle = driver.getTitle();
+        Assert.assertEquals(actualTitle,expectTitleAfterClicking,"New window title verification failed!");
 
     }
 }
