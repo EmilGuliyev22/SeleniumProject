0875c340563afef228796ae46b0a0c2d614c6007
Aysun120 <aysundamar114@gmail.com>
2022-06-14 18:30:52 -0500

    WebTableUtils class added

--- src/test/java/com/cydeo/test/utilities/WebTableUtils.java
+++ src/test/java/com/cydeo/test/utilities/WebTableUtils.java
@@ -15,9 +15,18 @@ public class WebTableUtils {
     //as a String.
 
       public static String returnOrderDate(WebDriver driver,String customerName){
-       String locator ="//td[.='" + customerName + "']//following-sibling::td[.='12/31/2021']";
+       String locator ="//td[.='" + customerName + "']//following-sibling::td[3]";
+
           WebElement customerDateCell = driver.findElement(By.xpath(locator));
           return customerDateCell.getText();
       }
+
+    public static String returnOrderDate2(WebDriver driver,String customerName){
+        return driver.findElement(By.xpath("//td[.='" + customerName + "']//following-sibling::td[3]")).getText();
+    }
+
+
+
+
 
 }
