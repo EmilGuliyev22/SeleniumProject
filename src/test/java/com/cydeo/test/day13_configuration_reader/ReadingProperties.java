a962859a1516e913cc83f972f7a01158f8f0e450
Aysun120 <aysundamar114@gmail.com>
2022-06-16 18:39:12 -0500

    ReadingProperties class added

--- src/test/java/com/cydeo/test/day13_configuration_reader/ReadingProperties.java
+++ src/test/java/com/cydeo/test/day13_configuration_reader/ReadingProperties.java
@@ -2,10 +2,21 @@ package com.cydeo.test.day13_configuration_reader;
 
 import org.testng.annotations.Test;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.Properties;
+
 public class ReadingProperties {
 
     @Test
-    public void reading_from_properties_file(){
+    public void reading_from_properties_file() throws FileNotFoundException {
+
+        // Create Properties class' object.
+        // We need properties class to use getProperty("key") method
+        Properties properties = new Properties();
+
+      // Create FileInputStream object to open file as a stream in Java memory.
+       FileInputStream file = new FileInputStream("configuration.properties");
 
 
     }
