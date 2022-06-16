54d49f8863546d19cb2585c9bffa9339bbd7c8dd
Aysun120 <aysundamar114@gmail.com>
2022-06-16 18:24:59 -0500

    ReadingProperties class added

---
+++ src/test/java/com/cydeo/test/day13_configuration_reader/ReadingProperties.java
@@ -0,0 +1,12 @@
+package com.cydeo.test.day13_configuration_reader;
+
+import org.testng.annotations.Test;
+
+public class ReadingProperties {
+
+    @Test
+    public void reading_from_properties_file(){
+
+
+    }
+}
