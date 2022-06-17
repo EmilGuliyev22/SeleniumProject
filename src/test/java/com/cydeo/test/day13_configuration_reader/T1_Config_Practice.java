cbaaa4d7bf5690798550ec9c20a8a1b357dffe5f
Aysun120 <aysundamar114@gmail.com>
2022-06-16 20:13:45 -0500

    T1_Config_Practice class added

---
+++ src/test/java/com/cydeo/test/day13_configuration_reader/T1_Config_Practice.java
@@ -0,0 +1,25 @@
+package com.cydeo.test.day13_configuration_reader;
+
+import com.cydeo.test.base.TestBase;
+import com.google.common.base.Verify;
+import org.apache.commons.logging.Log;
+import org.testng.annotations.Test;
+
+public class T1_Config_Practice extends TestBase {
+
+
+    @Test
+    public void login_crm_app(){
+//        2. Go to : http://login1.nextbasecrm.com/
+
+//        3. Enter valid username
+
+//        4. Enter valid password
+
+//        5. Click to Log In button
+
+//        6. Verify title is as expected:
+//        Expected: Portal
+
+    }
+}
