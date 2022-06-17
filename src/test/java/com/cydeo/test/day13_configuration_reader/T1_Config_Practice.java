2b47e0f69f0f74a9285961572e5f2c0a6fa8ea76
Aysun120 <aysundamar114@gmail.com>
2022-06-16 20:27:42 -0500

    T1_Config_Practice class added

--- src/test/java/com/cydeo/test/day13_configuration_reader/T1_Config_Practice.java
+++ src/test/java/com/cydeo/test/day13_configuration_reader/T1_Config_Practice.java
@@ -1,8 +1,11 @@
 package com.cydeo.test.day13_configuration_reader;
 
 import com.cydeo.test.base.TestBase;
+import com.cydeo.test.utilities.BrowserUtils;
 import com.google.common.base.Verify;
 import org.apache.commons.logging.Log;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.testng.annotations.Test;
 
 public class T1_Config_Practice extends TestBase {
@@ -10,16 +13,24 @@ public class T1_Config_Practice extends TestBase {
 
     @Test
     public void login_crm_app(){
-//        2. Go to : http://login1.nextbasecrm.com/
+//        2. Go to : https://login1.nextbasecrm.com/
+        driver.get("https://login2.nextbasecrm.com/");
 
 //        3. Enter valid username
+        WebElement inputUsername = driver.findElement(By.xpath("//input[@name='USER_LOGIN']"));
+        inputUsername.sendKeys("helpdesk1@cybertekschool.com");
 
 //        4. Enter valid password
+        WebElement inputPassword = driver.findElement(By.xpath("//input[@name='USER_PASSWORD']"));
+        inputPassword.sendKeys("UserUser");
 
 //        5. Click to Log In button
+        WebElement loginButton = driver.findElement(By.xpath("//input[@value='Log In']"));
+        loginButton.click();
 
 //        6. Verify title is as expected:
 //        Expected: Portal
+    //    BrowserUtils.verifyTitle(driver,"(1) Portal");
 
     }
 }
