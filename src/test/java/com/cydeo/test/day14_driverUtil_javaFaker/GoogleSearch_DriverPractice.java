1be6583d898aa4d0f131a4eb63a59b240a65e21a
Aysun120 <aysundamar114@gmail.com>
2022-06-18 11:48:38 -0500

    GoogleSearch_DriverPractice class added

--- src/test/java/com/cydeo/test/day14_driverUtil_javaFaker/GoogleSearch_DriverPractice.java
+++ src/test/java/com/cydeo/test/day14_driverUtil_javaFaker/GoogleSearch_DriverPractice.java
@@ -1,5 +1,12 @@
 package com.cydeo.test.day14_driverUtil_javaFaker;
 
+import com.cydeo.test.utilities.ConfigurationReader;
+import com.cydeo.test.utilities.Driver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class GoogleSearch_DriverPractice {
@@ -8,13 +15,17 @@ public class GoogleSearch_DriverPractice {
     public void google_search_driver_practice(){
 
         // Go to "https://www.google.com/"
-
+        Driver.getDriver().get(ConfigurationReader.getProperty("google.url"));
 
         // Write “apple” in search box
-
+        WebElement searchBox = Driver.getDriver().findElement(By.name("q"));
+        searchBox.sendKeys("apple" + Keys.ENTER);
 
         // Verify title:
         //Expected: apple - Google Search
+        String actualTitle = Driver.getDriver().getTitle();
+        String expectedTitle= "apple - Google Search";
+        Assert.assertEquals(actualTitle,expectedTitle);
 
     }
 }
