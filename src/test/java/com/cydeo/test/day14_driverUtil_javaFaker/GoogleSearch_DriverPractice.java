471e52a3426d912e963a2f46db110cb8a4e7b0bd
Aysun120 <aysundamar114@gmail.com>
2022-06-18 11:54:44 -0500

    GoogleSearch_DriverPractice class added

--- src/test/java/com/cydeo/test/day14_driverUtil_javaFaker/GoogleSearch_DriverPractice.java
+++ src/test/java/com/cydeo/test/day14_driverUtil_javaFaker/GoogleSearch_DriverPractice.java
@@ -6,6 +6,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -16,14 +17,17 @@ public class GoogleSearch_DriverPractice {
 
         // Go to "https://www.google.com/"
         Driver.getDriver().get(ConfigurationReader.getProperty("google.url"));
+        System.out.println("((RemoteWebDriver) Driver.getDriver()).getSessionId() = " + ((RemoteWebDriver) Driver.getDriver()).getSessionId());
 
         // Write “apple” in search box
         WebElement searchBox = Driver.getDriver().findElement(By.name("q"));
+        System.out.println("((RemoteWebDriver) Driver.getDriver()).getSessionId() = " + ((RemoteWebDriver) Driver.getDriver()).getSessionId());
         searchBox.sendKeys("apple" + Keys.ENTER);
 
         // Verify title:
         //Expected: apple - Google Search
         String actualTitle = Driver.getDriver().getTitle();
+        System.out.println("((RemoteWebDriver) Driver.getDriver()).getSessionId() = " + ((RemoteWebDriver) Driver.getDriver()).getSessionId());
         String expectedTitle= "apple - Google Search";
         Assert.assertEquals(actualTitle,expectedTitle);
 
