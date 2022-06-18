70830e0c3a748e24c5da2168680aa7a5308daaa8
Aysun120 <aysundamar114@gmail.com>
2022-06-18 11:30:48 -0500

    GoogleSearch_DriverPractice class added

---
+++ src/test/java/com/cydeo/test/day14_driverUtil_javaFaker/GoogleSearch_DriverPractice.java
@@ -0,0 +1,20 @@
+package com.cydeo.test.day14_driverUtil_javaFaker;
+
+import org.testng.annotations.Test;
+
+public class GoogleSearch_DriverPractice {
+
+    @Test
+    public void google_search_driver_practice(){
+
+        // Go to "https://www.google.com/"
+
+
+        // Write “apple” in search box
+
+
+        // Verify title:
+        //Expected: apple - Google Search
+
+    }
+}
