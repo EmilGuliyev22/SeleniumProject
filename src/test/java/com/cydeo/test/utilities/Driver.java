cecb2272ac551f4f6aa63ca408563352a4b646bb
Aysun120 <aysundamar114@gmail.com>
2022-06-18 09:51:49 -0500

    Driver class added

--- src/test/java/com/cydeo/test/utilities/Driver.java
+++ src/test/java/com/cydeo/test/utilities/Driver.java
@@ -6,18 +6,25 @@ public class Driver {
 
     // Creating a private constructor, we are closing access to the
     // object of this class from outside the class
-    private  Driver(){}
+    private Driver() {
+    }
 
     // We make WebDriver private, because we want to close access from outside of class
     // We make it static, because we will use it inside static method
     private static WebDriver driver;
 
-    public static WebDriver getDriver(){
+
+    public static WebDriver getDriver() {
+
+       // it will check if driver is null and if it is we will set up browser inside if statiement
+        // if you already setup driver and using it again for following line of codes, it will return to same driver
+        if (driver == null) {
+
+
+        }
+
+        return driver;
 
 
     }
-
-
-
-
 }
