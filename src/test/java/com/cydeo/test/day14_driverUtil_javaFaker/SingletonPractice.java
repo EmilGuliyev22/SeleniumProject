73f3450e876b938bb2992c092646d6bd5ab835f6
Aysun120 <aysundamar114@gmail.com>
2022-06-18 10:49:47 -0500

    SingletonPractice class added

---
+++ src/test/java/com/cydeo/test/day14_driverUtil_javaFaker/SingletonPractice.java
@@ -0,0 +1,14 @@
+package com.cydeo.test.day14_driverUtil_javaFaker;
+
+import org.testng.annotations.Test;
+
+public class SingletonPractice {
+
+    @Test
+    public void singleton_understanding_test(){
+
+
+
+
+    }
+}
