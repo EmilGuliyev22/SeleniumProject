aa611bf136a869f90411dae9031d9179d8f20976
Aysun120 <aysundamar114@gmail.com>
2022-06-21 18:33:09 -0500

    T1_JavaFaker_Registration_Form class added

---
+++ src/test/java/com/cydeo/test/day15_upload_actions/T1_JavaFaker_Registration_Form.java
@@ -0,0 +1,52 @@
+package com.cydeo.test.day15_upload_actions;
+
+import com.cydeo.test.utilities.ConfigurationReader;
+import com.cydeo.test.utilities.Driver;
+import com.github.javafaker.Faker;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.annotations.Test;
+
+public class T1_JavaFaker_Registration_Form {
+
+    @Test
+    public void registration_form_test(){
+
+        //    Note: Use JavaFaker OR read from configuration.properties file when possible.
+//    1. Open browser
+//    2. Go to website: https://practice.cydeo.com/registration_form
+     // driver.get("https://practice.cydeo.com/registration_form");
+        Driver.getDriver().get(ConfigurationReader.getProperty("registration.form.url"));
+
+        Faker faker = new Faker();
+
+//    3. Enter first name
+        WebElement firstname = Driver.getDriver().findElement(By.name("firstname"));
+        firstname.sendKeys(faker.name().firstName());
+
+//    4. Enter last name
+
+//    5. Enter username
+
+//    6. Enter email address
+
+//    7. Enter password
+
+//    8. Enter phone number
+
+//    9. Select a gender from radio buttons
+
+//    10.Enter date of birth
+
+//    11.Select Department/Office
+
+
+
+//    12.Select Job Title
+//    13.Select programming language from checkboxes
+//    14.Click to sign up button
+//    15.Verify success message “You’ve successfully completed registration.” is
+//    displayed.
+
+    }
+}
