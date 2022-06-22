d7a1fadebe7db7d5d6997cbcddd2603b1845678d
Aysun120 <aysundamar114@gmail.com>
2022-06-21 19:24:59 -0500

    T2_Guru99_Upload_Test class added

---
+++ src/test/java/com/cydeo/test/day15_upload_actions/T2_Guru99_Upload_Test.java
@@ -0,0 +1,24 @@
+package com.cydeo.test.day15_upload_actions;
+
+import org.testng.annotations.Test;
+
+public class T2_Guru99_Upload_Test {
+
+    @Test
+    public void upload_file_test(){
+
+        //    1. Go to “http://demo.guru99.com/test/upload”
+
+//    2. Upload file into Choose File
+
+//    3. Click terms of service checkbox
+
+//    4. Click Submit File button
+
+
+//    5. Verify expected message appeared.
+//    Expected: “1 file
+//    has been successfully uploaded.”
+
+    }
+}
