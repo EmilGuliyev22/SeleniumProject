707c13e52b548a259a3c81524cb5912a53a1f75c
Aysun120 <aysundamar114@gmail.com>
2022-06-22 18:24:01 -0500

    T3_Actions_Hover_Practice class added

--- src/test/java/com/cydeo/test/day16_actions_jsexecutor/T1_Guru99_DragandDrop_Test.java
+++ src/test/java/com/cydeo/test/day16_actions_jsexecutor/T1_Guru99_DragandDrop_Test.java
@@ -1,5 +1,9 @@
 package com.cydeo.test.day16_actions_jsexecutor;
 
+import com.cydeo.test.utilities.ConfigurationReader;
+import com.cydeo.test.utilities.Driver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.testng.annotations.Test;
 
 public class T1_Guru99_DragandDrop_Test {
@@ -9,9 +13,11 @@ public class T1_Guru99_DragandDrop_Test {
 
         //    1- Open a chrome browser
 //    2- Go to: https://demo.guru99.com/test/drag_drop.html
+        Driver.getDriver().get(ConfigurationReader.getProperty("guru99.dropdown.url"));
 
 //    3- Drag “BANK” and drop into Account area under Debit Side
-
+        WebElement bank = Driver.getDriver().findElement(By.partialLinkText("BANK"));
+        WebElement debitAccount = Driver.getDriver().findElement(By.id("bank"));
 
 //    4- Drag “5000” and drop into Amount area under Debit Side
 
