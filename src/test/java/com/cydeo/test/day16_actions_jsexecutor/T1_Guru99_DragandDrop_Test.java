76d3a67b167a6e97292abb77165f82ce3327f701
Aysun120 <aysundamar114@gmail.com>
2022-06-22 18:12:00 -0500

    T3_Actions_Hover_Practice class added

---
+++ src/test/java/com/cydeo/test/day16_actions_jsexecutor/T1_Guru99_DragandDrop_Test.java
@@ -0,0 +1,27 @@
+package com.cydeo.test.day16_actions_jsexecutor;
+
+import org.testng.annotations.Test;
+
+public class T1_Guru99_DragandDrop_Test {
+
+    @Test
+    public void guru99_dragAndDrop_test(){
+
+        //    1- Open a chrome browser
+//    2- Go to: https://demo.guru99.com/test/drag_drop.html
+
+//    3- Drag “BANK” and drop into Account area under Debit Side
+
+
+//    4- Drag “5000” and drop into Amount area under Debit Side
+
+
+//    5- Drag “SALES” and drop into Account area under Credit Side
+
+
+//    6- Drag “5000” and drop into Amount area under Credit Side
+
+
+//    7- Verify “Perfect!” text displayed.
+    }
+}
