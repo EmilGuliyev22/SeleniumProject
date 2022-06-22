f7678d936e44000ddeca606509bc22a379b2aad4
Aysun120 <aysundamar114@gmail.com>
2022-06-22 18:48:30 -0500

    T3_Actions_Hover_Practice class added

--- src/test/java/com/cydeo/test/day16_actions_jsexecutor/T2_Actions_Scroll.java
+++ src/test/java/com/cydeo/test/day16_actions_jsexecutor/T2_Actions_Scroll.java
@@ -1,4 +1,26 @@
 package com.cydeo.test.day16_actions_jsexecutor;
 
+import org.testng.annotations.Test;
+
 public class T2_Actions_Scroll {
+
+    @Test
+    public void actions_scroll_test(){
+
+        //    1- Open a chrome browser
+//    2- Go to: https://practice.cydeo.com/
+
+//    3- Scroll down to “Powered by CYDEO”
+
+//    4- Scroll using Actions class “moveTo(element)” method
+
+
+
+
+
+//    1- Continue from where previous task left in the same test.
+
+//    2- Scroll back up to “Home” link using PageUP button
+
+    }
 }
