6d6472a60475eb904064f4030f7890dee447fbd3
Aysun120 <aysundamar114@gmail.com>
2022-06-22 20:54:49 -0500

    T3_JSExecutor_Scroll class added

--- src/test/java/com/cydeo/test/day16_actions_jsexecutor/T3_JSExecutor_Scroll.java
+++ src/test/java/com/cydeo/test/day16_actions_jsexecutor/T3_JSExecutor_Scroll.java
@@ -2,10 +2,13 @@ package com.cydeo.test.day16_actions_jsexecutor;
 
 import com.cydeo.test.utilities.ConfigurationReader;
 import com.cydeo.test.utilities.Driver;
+import com.github.javafaker.Faker;
 import com.google.common.base.Verify;
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class T3_JSExecutor_Scroll {
@@ -25,13 +28,14 @@ public class T3_JSExecutor_Scroll {
         js.executeScript("arguments[0].scrollIntoView(true)",email);
 
 //         Generate random email and enter into subscribe box
-
-//         Click on Subscribe
+        //         Click on Subscribe
+        Faker faker = new Faker();
+        email.sendKeys(faker.internet().emailAddress() + Keys.ENTER);
 
 
 //         Verify "Great! We've sent you an email to confirm your subscription." is displayed
-
-
+          WebElement resultMsg = Driver.getDriver().findElement(By.xpath("//div[@class='wt-alert wt-alert--inline wt-alert--success-01 wt-text-body-01']"));
+        Assert.assertEquals(resultMsg.getText(),"Great! We've sent you an email to confirm your subscription.");
 
     }
 }
