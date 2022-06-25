9dcc20cd76e5f2476275f1a5a20bbf19b51b3fae
Aysun120 <aysundamar114@gmail.com>
2022-06-25 11:38:54 -0500

    CydeoPracticePage class added

--- src/test/java/com/cydeo/test/day17_pom_syncronization/POM_ActionsPractices.java
+++ src/test/java/com/cydeo/test/day17_pom_syncronization/POM_ActionsPractices.java
@@ -1,6 +1,10 @@
 package com.cydeo.test.day17_pom_syncronization;
 
+import com.cydeo.pages.CydeoPracticePage;
+import com.cydeo.test.utilities.Driver;
 import com.google.common.base.Verify;
+import org.openqa.selenium.By;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class POM_ActionsPractices {
@@ -10,10 +14,15 @@ public class POM_ActionsPractices {
 //        1. Open a chrome browser
 //        2. Go to:
 //        https://practice.cydeo.com/drag_and_drop_circles
+        Driver.getDriver().get("https://practice.cydeo.com/drag_and_drop_circles");
 
+        CydeoPracticePage cydeoPracticePage = new CydeoPracticePage();
 
 //        3. Verify expected default text appears on big circle
 //                Expected = "Drag the small circle here."
+
+        Assert.assertEquals(cydeoPracticePage.bigCircle.getText(),"Drag the small circle here.");
+
     }
 
 
