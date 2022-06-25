83352e2e5c520bceca9a3e853332a4425f488c8c
Aysun120 <aysundamar114@gmail.com>
2022-06-25 14:54:36 -0500

    DynamicallyLoadedTest class added

--- src/test/java/com/cydeo/test/day17_pom_syncronization/DynamicallyLoadedTest.java
+++ src/test/java/com/cydeo/test/day17_pom_syncronization/DynamicallyLoadedTest.java
@@ -1,5 +1,21 @@
 package com.cydeo.test.day17_pom_syncronization;
 
+import org.apache.hc.core5.http.Message;
+import org.openqa.selenium.support.ui.Wait;
+import org.testng.annotations.Test;
+
 public class DynamicallyLoadedTest {
 
+    @Test
+    public void dynamically_load_page_7(){
+
+//        1. Go to https://practice.cydeo.com/dynamic_loading/7
+
+
+//        2. Wait until title is “Dynamic title”
+//        3. Assert:
+//        Message “Done” is displayed.
+//        4. Assert: Image is displayed.
+
+    }
 }
