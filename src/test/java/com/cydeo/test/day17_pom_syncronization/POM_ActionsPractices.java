e6ca3da846c1b04e7b3ae1bad950d3e4e97ef902
Aysun120 <aysundamar114@gmail.com>
2022-06-25 10:37:09 -0500

    POM_ActionsPractices class added

---
+++ src/test/java/com/cydeo/test/day17_pom_syncronization/POM_ActionsPractices.java
@@ -0,0 +1,18 @@
+package com.cydeo.test.day17_pom_syncronization;
+
+import com.google.common.base.Verify;
+import org.testng.annotations.Test;
+
+public class POM_ActionsPractices {
+
+    @Test
+    public void drag_small_circle_text(){
+//        1. Open a chrome browser
+//        2. Go to:
+//        https://practice.cydeo.com/drag_and_drop_circles
+
+
+//        3. Verify expected default text appears on big circle
+//                Expected = "Drag the small circle here."
+    }
+}
