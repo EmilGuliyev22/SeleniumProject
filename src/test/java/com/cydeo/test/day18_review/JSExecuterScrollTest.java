4772c2091d5c2670adc323a3f561400071b3ff2a
Aysun120 <aysundamar114@gmail.com>
2022-06-26 12:07:44 -0500

    JSExecuterScrollPage class added

--- src/test/java/com/cydeo/test/day18_review/JSExecuterScrollTest.java
+++ src/test/java/com/cydeo/test/day18_review/JSExecuterScrollTest.java
@@ -1,21 +1,30 @@
 package com.cydeo.test.day18_review;
 
+import com.cydeo.test.utilities.Driver;
+import org.openqa.selenium.JavascriptExecutor;
 import org.testng.annotations.Test;
 
 public class JSExecuterScrollTest {
 
     @Test
-    public void js_executer_scroll_test(){
+    public void js_executer_scroll_test() throws InterruptedException {
+
 //        1- Open a chrome browser
 //        2- Go to: https://practice.cydeo.com/infinite_scroll
-
+        Driver.getDriver().get("https://practice.cydeo.com/infinite_scroll");
 
 
 //        3- Use below JavaScript method and scroll
+        JavascriptExecutor js = (JavascriptExecutor) Driver.getDriver();
+        Thread.sleep(3000);
+        js.executeScript("window.scrollBy(0,7500)"); // window.scrollBy(x,y)
 
 
 
 //        a.  750 pixels down 10 times.
+
+
+
 //                b.  750 pixels up 10 times
 
 
