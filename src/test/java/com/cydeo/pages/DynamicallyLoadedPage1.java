c86796f78a36557f91382eefee69571f1c433040
Aysun120 <aysundamar114@gmail.com>
2022-06-26 10:33:27 -0500

    DynamicallyLoadedPage1 class added

--- src/test/java/com/cydeo/pages/DynamicallyLoadedPage1.java
+++ src/test/java/com/cydeo/pages/DynamicallyLoadedPage1.java
@@ -9,14 +9,6 @@ import org.testng.Assert;
 
 public class DynamicallyLoadedPage1 {
 
-//    1. Go to https://practice.cydeo.com/dynamic_loading/1
-//            2. Click to start
-//3. Wait until loading bar disappears
-//4. Assert username inputbox is displayed
-//5. Enter username: tomsmith
-//6. Enter password: incorrectpassword
-//7. Click to Submit button
-//8. Assert “Your password is invalid!” text is displayed.
 
     public DynamicallyLoadedPage1(){
         PageFactory.initElements(Driver.getDriver(),this);
---
+++ src/test/java/com/cydeo/test/day18_review/DynamicallyLoadedTest1.java
@@ -0,0 +1,20 @@
+package com.cydeo.test.day18_review;
+
+import org.testng.annotations.Test;
+
+public class DynamicallyLoadedTest1 {
+
+    @Test
+    public void dynamically_load_page_1(){
+        //    1. Go to https://practice.cydeo.com/dynamic_loading/1
+
+//            2. Click to start
+
+//3. Wait until loading bar disappears
+//4. Assert username inputbox is displayed
+//5. Enter username: tomsmith
+//6. Enter password: incorrectpassword
+//7. Click to Submit button
+//8. Assert “Your password is invalid!” text is displayed.
+    }
+}
