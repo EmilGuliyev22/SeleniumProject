977d855f94872d85fd49109566a22c04895ca1c3
Aysun120 <aysundamar114@gmail.com>
2022-06-26 10:44:13 -0500

    DynamicallyLoadedTest1 class added

--- src/test/java/com/cydeo/test/day18_review/DynamicallyLoadedTest1.java
+++ src/test/java/com/cydeo/test/day18_review/DynamicallyLoadedTest1.java
@@ -1,5 +1,10 @@
 package com.cydeo.test.day18_review;
 
+import com.cydeo.pages.DynamicallyLoadedPage1;
+import com.cydeo.test.utilities.Driver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.Test;
 
 public class DynamicallyLoadedTest1 {
@@ -7,10 +12,17 @@ public class DynamicallyLoadedTest1 {
     @Test
     public void dynamically_load_page_1(){
         //    1. Go to https://practice.cydeo.com/dynamic_loading/1
+        Driver.getDriver().get("https://practice.cydeo.com/dynamic_loading/1");
+        DynamicallyLoadedPage1 dynamicallyLoadedPage1 = new DynamicallyLoadedPage1();
 
 //            2. Click to start
+        dynamicallyLoadedPage1.startBtn.click();
 
 //3. Wait until loading bar disappears
+        WebDriverWait wait = new WebDriverWait(Driver.getDriver(),10);
+        wait.until(ExpectedConditions.invisibilityOf(dynamicallyLoadedPage1.loadingBar));
+
+
 //4. Assert username inputbox is displayed
 //5. Enter username: tomsmith
 //6. Enter password: incorrectpassword
